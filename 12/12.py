14288fc6ee5a9266f459dcbf7918fcb951b200e7
n.e.hullegien@student.tudelft.nl <n.e.hullegien@student.tudelft.nl>
2022-12-13 17:18:25 +0100

    12 (waarom moeilijk doen als het ook makkelijk kan)

--- 12/12.py
+++ 12/12.py
@@ -1,46 +1,49 @@
-def bfs(grid):
-    bfsQueue = [((0, 0), [])]
-    visited = set()
-    maxVal = -1
-    while bfsQueue:
-        currentPos, path = bfsQueue.pop(0)
-        currentVal = grid[currentPos]
-        if currentVal > maxVal:
-            maxVal = currentVal
-            # print('NEW HIGHEST VAL:', maxVal)
-        visited.add(currentPos)
-        if currentVal == 27:
-            return path + [currentPos]
+import networkx as nx
+from networkx import NetworkXNoPath
 
+grid = {}
+graph = nx.DiGraph()
+start = None
+starts = []
+end = None
+with open('./input.txt') as f:
+    lines = [list(line.strip('\n')) for line in f]
+
+    for i, line in enumerate(lines):
+        for j, elem in enumerate(line):
+            graph.add_node((i, j))
+            if elem == 'a':
+                starts.append((i, j))
+            if elem == 'S':
+                start = (i, j)
+                starts.append((i, j))
+                grid[(i, j)] = 1
+            elif elem == 'E':
+                end = (i, j)
+                grid[(i, j)] = 26
+            else:
+                grid[(i, j)] = ord(elem) - 96
+    for coord in grid:
+        currentVal = grid[coord]
         adjacentCoords = list(
             filter(lambda z: currentVal >= grid[z] or grid[z] - currentVal == 1,
-                   filter(lambda y: not visited.__contains__(y),
-                          filter(lambda x: grid.__contains__(x),
-                                 filter(lambda w: not path.__contains__(w), [
-                                     (currentPos[0] - 1, currentPos[1]),
-                                     (currentPos[0] + 1, currentPos[1]),
-                                     (currentPos[0], currentPos[1] - 1),
-                                     (currentPos[0], currentPos[1] + 1)
-                                 ])))))
-        print("CURRENT VALUE", currentVal, "CURRENTPOS", currentPos, 'ADJACENT', adjacentCoords, 'PATH:', path, 'VISITED', len(visited))
+                   filter(lambda x: grid.__contains__(x), [
+                              (coord[0] - 1, coord[1]),
+                              (coord[0] + 1, coord[1]),
+                              (coord[0], coord[1] - 1),
+                              (coord[0], coord[1] + 1)
+                          ])))
+        for adjacentCoord in adjacentCoords:
+            graph.add_edge(coord, adjacentCoord)
+    print('12a:', nx.dijkstra_path_length(graph, start, end))
+    minDist = 90000000
+    for x in starts:
+        try:
+            minDist = min(minDist, nx.dijkstra_path_length(graph, x, end))
+        except NetworkXNoPath:
+            continue
 
-        for coord in adjacentCoords:
-            bfsQueue.append((coord, path + [coord]))
 
-    return None
+    print('12b:', minDist)
 
 
-if __name__ == '__main__':
-    grid = {}
-    with open('./example.txt') as f:
-        lines = [list(line.strip('\n')) for line in f]
-        for i, line in enumerate(lines):
-            for j, elem in enumerate(line):
-                if elem == 'S':
-                    grid[(i, j)] = 1
-                elif elem == 'E':
-                    grid[(i, j)] = 27
-                else:
-                    grid[(i, j)] = ord(elem) - 96
-    print(grid)
-    print(len(bfs(grid)))
